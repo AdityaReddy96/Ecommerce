4e370b68d9d936fef83dec13479938d4579cf531
Aditya Reddy <adityareddy393@gmail.com>
2025-07-05 13:59:33 +0530

    shopping view store handled

--- client/src/store/store.js
+++ client/src/store/store.js
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "./auth-slice";
-import adminProductSlice from "./admin/product-slice";
+import adminProductSlice from "./admin/admin-product-slice";
+import shopProductSlice from "./shop/shop-products-silce";
 
 const store = configureStore({
   reducer: {
     auth: authReducer,
     adminProducts: adminProductSlice,
+    shopProducts: shopProductSlice,
   },
 });
 
